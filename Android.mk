73bd6c556807a0ed3b2faa58d0dfb69b4e04342d
Sireesh Tripurari <sireesh.tripurari@linaro.org>
2014-04-30 14:48:54 +0530

    enable NEON optimizations
    use ARCH_ARM_HAVE_NEON for determining NEON support
    Remove unwanted files from libpng-1.6.10
    
    Change-Id: I45f4e863d06a4f6134419ee39c3866061d7873c6
    Signed-off-by: Sireesh Tripurari <sireesh.tripurari@linaro.org>

--- Android.mk
+++ Android.mk
@@ -21,19 +21,33 @@ common_SRC_FILES := \
 	pngwutil.c \
 
 # Previously these arm-specific flags were never applied.
-# TODO: apply the flags and fix the build.
-# my_cflags_arm := -DPNG_ARM_NEON_OPT=2 -DPNG_ARM_NEON_CHECK_SUPPORTED
-my_cflags_arm :=
+
+my_cflags_arm := -DPNG_ARM_NEON_CHECK_SUPPORTED
+
+#my_cflags_arm :=
 
 # BUG: http://llvm.org/PR19472 - SLP vectorization (on ARM at least) crashes
 # when we can't lower a vectorized bswap.
-my_cflags_arm += -fno-slp-vectorize
+my_cflags_arm += -fno-slp-vectorize 
 
+# filter_neon_intrinsics.c requires -mfpu=neon
 my_src_files_arm := \
-    arm/arm_init.c \
-    arm/filter_neon.S \
-    arm/filter_neon_intrinsics.c
+    arm/arm_init.c 
 
+
+ifeq ($(ARCH_ARM_HAVE_NEON),true)
+	my_cflags_arm += -DPNG_ARM_NEON_OPT=1 
+	my_src_files_arm += \
+		arm/filter_neon_intrinsics.c
+else
+#handcoded assembly source to be used
+	my_cflags_arm += -DPNG_ARM_NEON_OPT=2 
+	my_src_files_arm +=\ 
+		arm/filter_neon.c
+endif
+
+my_src_files_arm64:=\
+	$(my_src_files_arm)
 
 common_CFLAGS := -std=gnu89 #-fvisibility=hidden ## -fomit-frame-pointer
 
@@ -82,9 +96,8 @@ LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
 LOCAL_CFLAGS_arm := $(my_cflags_arm)
-LOCAL_ASFLAGS += $(common_ASFLAGS)
+LOCAL_ASFLAGS += $(common_ASFLAGS) $(my_cflags_arm)
 LOCAL_SRC_FILES_arm := $(my_src_files_arm)
-
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
 	external/zlib
 LOCAL_SHARED_LIBRARIES := \
@@ -102,7 +115,7 @@ LOCAL_CLANG := true
 LOCAL_SRC_FILES := $(common_SRC_FILES)
 LOCAL_CFLAGS += $(common_CFLAGS) -ftrapv
 LOCAL_CFLAGS_arm := $(my_cflags_arm)
-LOCAL_ASFLAGS += $(common_ASFLAGS)
+LOCAL_ASFLAGS += $(common_ASFLAGS) $(my_cflags_arm)
 LOCAL_SRC_FILES_arm := $(my_src_files_arm)
 
 LOCAL_C_INCLUDES += $(common_C_INCLUDES) \
